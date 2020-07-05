779d5141d971318b0ef22aa3718537980732d3b5
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2020-07-05 16:28:57 +0700

    Some misc notebooks

--- web/views.py
+++ web/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from web.models import Greeting
 
 import matplotlib.pyplot as plt
 import mpld3
